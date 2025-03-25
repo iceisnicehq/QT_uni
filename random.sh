8b821d21e5d57caae565cf4264c455ef1555077d
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 17:32:24 +0300

    Add files via upload

---
+++ random.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Function to generate a random ANSI color code (bold, bright colors)
+random_color() {
+    colors=("31" "32" "33" "34" "35" "36" "91" "92" "93" "94" "95" "96")
+    echo -e "\033[1;${colors[$((RANDOM % ${#colors[@]}))]}m"
+}
+
+# Reset terminal color
+reset_color="\033[0m"
+
+# Main loop
+while true; do
+    color=$(random_color)
+    echo -e "${color}СабировДС-КС2203
+    ᓚᘏᗢ 
+    /\\_/\\
+   ( o.o )
+    > ^ <${reset_color}"
+    sleep 3
+done
